51940e1f5a173dce64a7a8a03bb2a96f9e5bde2b
Jonathan Klabunde Tomer <jonathantomer@gmail.com>
2019-02-11 21:23:46 +0000

    ultipro_google: allow for missing YTD column in earnings
    Some of my pay statements do not have a YTD column for statements where there
    are two rows with the same earnings type.

--- beancount_import/source/ultipro_google_statement.py
+++ beancount_import/source/ultipro_google_statement.py
@@ -110,6 +110,12 @@ def parse(text: str) -> ParseResult:
                      ('Current', parse_currency),
                      ('YTD', parse_currency)),
                     #(r'^(Total Hours) (' + decimal_re + r')$', ('hours', D)),
+                    (r'^(' + field_name_re + r')[ \n](?:[0-5] [a-zA-Z ]+)?(' +
+                     decimal_re + r')' + 2 *
+                     (r' (' + currency_amount_re + r')') + r'$',
+                     ('Hours', D),
+                     ('Pay Rate', parse_currency),
+                     ('Current', parse_currency)),
                 ]),
             (
                 r'^(Earnings)\nPay Type Hours\nPay\nRate\nPiece\nUnits\nPiece\nRate Current YTD$',
