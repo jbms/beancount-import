03b9a06ac068b1d0b4c587ae942e52028dc2a4d4
Moritz Jung <18733473+moritzj29@users.noreply.github.com>
2021-12-18 22:18:47 +0100

    add some sanitized invoice tests for de_DE

--- beancount_import/source/amazon_invoice_test.py
+++ beancount_import/source/amazon_invoice_test.py
@@ -17,10 +17,33 @@ testdata_dir = os.path.abspath(
     '166-7926740-5141621',
     'D56-5204779-4181560',
 ])
-def test_parsing(name: str):
+def test_parsing_en_EN(name: str):
     source_path = os.path.join(testdata_dir, name + '.html')
     invoice = amazon_invoice.parse_invoice(source_path)
     json_path = os.path.join(testdata_dir, name + '.json')
+    expected = json.load(
+        open(json_path, 'r'), object_pairs_hook=collections.OrderedDict)
+    expected_str = json.dumps(expected, indent=4)
+    actual = amazon_invoice.to_json(invoice)
+    actual_str = json.dumps(actual, indent=4)
+    if expected_str != actual_str:
+        print(actual_str)
+    assert expected_str == actual_str
+
+
+@pytest.mark.parametrize('name', [
+    '256-0244967-2403944',
+    '393-2608279-9292916',
+    '898-5185906-0096901',
+    '974-6135682-9358749',
+    'D22-9220967-2566135',
+    'D60-9825125-4795642'
+])
+def test_parsing_de_DE(name: str):
+    testdata_dir_locale = os.path.join(testdata_dir, 'de_DE')
+    source_path = os.path.join(testdata_dir_locale, name + '.html')
+    invoice = amazon_invoice.parse_invoice(source_path, locale=amazon_invoice.LOCALES['de_DE']())
+    json_path = os.path.join(testdata_dir_locale, name + '.json')
     expected = json.load(
         open(json_path, 'r'), object_pairs_hook=collections.OrderedDict)
     expected_str = json.dumps(expected, indent=4)
